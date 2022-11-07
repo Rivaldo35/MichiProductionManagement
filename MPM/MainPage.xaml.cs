2997c4a1ff25d66e7c255879def81207fb2a8a27
Rivaldo <rivaldo.vola@leanup-it.com>
2022-11-07 02:47:34 +0100

    MPM Project structure

---
+++ MPM/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MPM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MPM_Libary/Platforms/iOS/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace MPM_Libary
+{
+    // All the code in this file is only included on iOS.
+    public class PlatformClass1
+    {
+    }
+}
